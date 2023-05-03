e5a65fa8e368181ae3805d1cb7fcc35636f77b2d
Beyhan <btstructure97@gmail.com>
2023-05-03 18:52:34 -0400

    Day3

---
+++ Day3/if_else_statements.py
@@ -0,0 +1,12 @@
+#conditionals
+
+# if condition:
+#     do this
+# else: 
+#     do this
+
+water_level = 50
+if water_level > 80:
+    print("Drain water")
+else: 
+    print("Continue")
---
+++ Day3/logical_operators.py
@@ -0,0 +1,22 @@
+#logical operaots
+
+#the and operator means both statements must be true
+
+a = 12
+b = 5
+
+if a > 10 and b < 7:
+    print("True")
+else: 
+    print("False")
+
+if a < 10 and b < 7:
+    print("True")
+else: 
+    print("False")
+
+#the or operator checks if one condition is true
+if a < 10 or b < 7:
+    print("True")
+else: 
+    print("False")
