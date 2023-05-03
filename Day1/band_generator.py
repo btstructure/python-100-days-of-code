2a398c76f4cc2577d09be3e913e87a54070734f6
Beyhan <btstructure97@gmail.com>
2023-05-02 20:11:34 -0400

    day 1 completed

--- Day1/band_generator.py
+++ Day1/band_generator.py
@@ -1,3 +1,3 @@
-question1 = input("Where did you live as a kid?: " )
-question2 = input("What was the name of your pet?: ")
-print("The name of your band is " + question1 + " " +question2)
+city = input("Where did you live as a kid?: " )
+pet = input("What was the name of your pet?: ")
+print("The name of your band is " + city + " " + pet)
---
+++ Day1/string_length.py
@@ -0,0 +1,10 @@
+#input - takes the users input as a string
+name = input("What is your name? " )
+#len (length) - gets the length of a string
+print(len(name))
+
+print("Hello " + name)
+
+#another way to do the above, goes from inner parentheses to outer
+name2 = len(input("What is your name? " ))
+print(name2)
