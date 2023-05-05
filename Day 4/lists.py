0500eed97169feb8658bd6b04c4f8080b4683cc6
Beyhan <btstructure97@gmail.com>
2023-05-05 19:34:35 -0400

    Day4

---
+++ Day 4/lists.py
@@ -0,0 +1,17 @@
+#list - a data structure, organizing and storing data in python, a set of square brackets with items stored inside
+
+#the values in the list have an index that starts with a 0
+state_of_americas = ["Delaware", "New Jersey", "Georgia"]
+
+print(state_of_americas)
+
+print(state_of_americas[1])
+
+
+
+#to manipulate the list you can use the index and then change the item
+
+state_of_americas[1] = "New York"
+
+print(state_of_americas)
+# ['Delaware', 'New York', 'Georgia']
---
+++ Day 4/random_module.py
@@ -0,0 +1,11 @@
+#importing the random module
+#a module is responsible for a certian functionality
+import random
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+
+#random() gives a float number between 0.00000 - 0.999999
+randomFloat = random.random()
+print(randomFloat)
