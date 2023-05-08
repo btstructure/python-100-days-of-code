0c08e7b01d3fc4d3c3b2a66640ffe4a0782cd59c
Beyhan <btstructure97@gmail.com>
2023-05-08 18:55:07 -0400

    day5 completed

--- Day 5/password_generator.py
+++ Day 5/password_generator.py
@@ -9,4 +9,40 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 
+#Easy way - will have the number of letters followed by symbol and followed by the number
 
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#     random_letters = random.choice(letters)
+#     password += random_letters
+
+# for char in range(1, nr_symbols + 1):
+#     random_symbols = random.choice(symbols)
+#     password += random_symbols
+
+# for char in range(1, nr_numbers + 1):
+#     random_numbers = random.choice(numbers)
+#     password += random_numbers
+
+# print(password)
+
+#hard way -  instead of adding into a string you add into a list
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+for char in range(1, nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(password)
