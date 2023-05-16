2842675348b270190edbed63a83e4279a8c95715
Beyhan <btstructure97@gmail.com>
2023-05-16 19:07:44 -0400

    not functioning deciphering a message

---
+++ Day 8/encryption.py
@@ -0,0 +1,22 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#Don't change the code above 👆
+
+
+def decipher(text, shift):
+    deciphered_text = ""
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position + shift
+        new_letter = alphabet[new_position]
+        deciphered_text += new_letter
+    print(f"The encoded text is {deciphered_text}")
+
+
+
+
+decipher(text=text, shift=shift)
