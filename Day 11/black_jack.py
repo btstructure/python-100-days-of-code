2f5aa8d3dab75b1bdb7964a9d5b98e64844dbefd
Beyhan <btstructure97@gmail.com>
2023-06-06 19:35:35 -0400

    Day 11

---
+++ Day 11/black_jack.py
@@ -0,0 +1,24 @@
+import random
+
+
+def deal_card():
+    """Returns random card from the deck"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card 
+
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    # new_card = deal_card()
+    # user_cards.append(new_card)
+
+    """Similar to above"""
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+
+def calculate_score(cards):
+    return sum(cards)
+
