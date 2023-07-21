21a3a661864f5fbdff586c427bf8f34944fc164a
Beyhan <btstructure97@gmail.com>
2023-07-21 17:06:55 -0400

    not running in VS

--- Day 16/main.py
+++ Day 16/main.py
@@ -7,7 +7,11 @@ from turtle import Turtle, Screen
 timmy = Turtle()
 
 print(timmy)
-
+timmy.shape("turtle")
+timmy.color('coral')
 my_screen = Screen
 
 print(my_screen.canvheight)
+#screen will only close/end when it is closed out
+my_screen.exitonclick()
+
